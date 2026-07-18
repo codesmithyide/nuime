87b36140c9e1e4e60cf40077ed03336555f46058
Xavier Leclercq <xavier@leclercq.dev>
2026-07-18 10:15:02 +0100

    Create Nuime CLI (#1)

---
+++ cli/src/CommandLineSpecification.hpp
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2026 Xavier Leclercq
+// SPDX-License-Identifier: MIT
+
+#ifndef GUARD_CODESMITHYIDE_NUIME_CLI_COMMANDLINESPECIFICATION_HPP
+#define GUARD_CODESMITHYIDE_NUIME_CLI_COMMANDLINESPECIFICATION_HPP
+
+#include <Ishiko/Configuration.hpp>
+
+namespace Nuime
+{
+    class CommandLineSpecification : public Ishiko::CommandLineSpecification
+    {
+    public:
+        CommandLineSpecification();
+    };
+}
+
+#endif

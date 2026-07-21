5b552ddfc731ede87926fcc8694925cddb91aa1c
Xavier Leclercq <xavier@leclercq.dev>
2026-07-21 18:18:54 +0100

    Add Engine class

--- include/Nuime/Engine.hpp
+++ include/Nuime/Engine.hpp
@@ -1,9 +1,7 @@
 // SPDX-FileCopyrightText: 2026 Xavier Leclercq
 // SPDX-License-Identifier: MIT
 
-#ifndef GUARD_NUIME_HPP
-#define GUARD_NUIME_HPP
-
-#include "Nuime/Engine.hpp"
+#ifndef GUARD_NUIME_ENGINE_HPP
+#define GUARD_NUIME_ENGINE_HPP
 
 #endif
